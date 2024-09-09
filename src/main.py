35d2ce19aa82e48b74e4773c95c3a68bbd0a9c15
gaoyu <gaoyuhang@kanzhun.com>
2024-09-09 14:21:15 +0800

    test

--- src/main.py
+++ src/main.py
@@ -2,25 +2,51 @@
 
 import os
 import subprocess
-
+from datetime import datetime
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def split_array(arr, chunk_size=500):
+    """
+    将数组按指定的大小进行分隔。
+
+    Args:
+        arr (list): 要分隔的数组。
+        chunk_size (int): 每个子数组的最大元素个数。
+
+    Returns:
+        list: 包含子数组的列表，每个子数组最多包含 chunk_size 个元素。
+    """
+    # 使用列表切片将数组按chunk_size大小分隔
+    return [arr[i:i + chunk_size] for i in range(0, len(arr), chunk_size)]
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    command = '/Users/gaoyu/Desktop/unused/bin/unused -class -path /Users/gaoyu/Desktop/Orange-RD.app'
-    result = os.popen(command).read()
-    lines = result.split('\n')
-    # result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    start_time = datetime.now()
 
-    # # 输出标准输出和标准错误
-    # print("Standard Output:", result.stdout)
-    # print("Standard Error:", result.stderr)
+    array = [f"UIViewControllerController{i}" for i in range(1, 20000)]
+    chunks = split_array(array, chunk_size=500)  # 将数组按 500 个元素进行分隔
 
-    print(result)
+    # 打印结果
+    for i, chunk in enumerate(chunks):
+        print(f"Chunk {i + 1}: {chunk[:5]}... (共 {len(chunk)} 个元素)")
+
+        command = 'ag -G ".+\.(h|m|mm|xml|xib)$" --ignore Pods --ignore *.framework -w "%s" %s' % (
+            '|'.join(chunk), "/Users/gaoyu/AppFatless/BossZP/Code/Project")
+        print(command)
+        lines = []
+        matched_classes = []
+        try:
+            r = os.popen(command).read()
+            lines = r.split('\n')
+        except:
+            print(command)
+
+    print(datetime.now() - start_time)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- src/unused_class.py
+++ src/unused_class.py
@@ -7,64 +7,98 @@ import time
 
 reghClass = re.compile('@implementation (.*?)')
 
+# 分隔数组
+def split_array(arr, chunk_size=800):
+    """
+    将数组按指定的大小进行分隔。
+
+    Args:
+        arr (list): 要分隔的数组。
+        chunk_size (int): 每个子数组的最大元素个数。
+
+    Returns:
+        list: 包含子数组的列表，每个子数组最多包含 chunk_size 个元素。
+    """
+    # 使用列表切片将数组按chunk_size大小分隔
+    return [arr[i:i + chunk_size] for i in range(0, len(arr), chunk_size)]
+
 def get_unused_class_fromipa(ipa_path=''):
     command = '../bin/unused -class -path %s' % ipa_path
     result = os.popen(command).read()
     return result.split('\n')
 
-def exists_in_private_modules(modules_dir='', classes=None):
-    if classes is None:
+def chunk_exists_in_private_modules(search_word_list, search_dir_list, chunk_size=800):
+    used_class_list = []
+    chunks = split_array(search_word_list, chunk_size)
+    for i, chunk in enumerate(chunks):
+        used_list = exists_in_private_modules(chunk, search_dir_list)
+        used_class_list.extend(used_list)
+    return used_class_list
+
+def exists_in_private_modules(search_word_list, search_dir_list):
+    if len(search_word_list) <= 0 or len(search_dir_list) <= 0:
         return []
-    temp = []
-    for cls in classes:
-        exist = False
-        command = 'ag -G ".+\.(m|mm)$" --ignore Pods --ignore *.framework "@implementation %s" %s' % (cls, modules_dir)
+    print("find class count: %s in %s" % (len(search_word_list), search_dir_list))
+    command = 'ag -G ".+\.(m|mm)$" --ignore Pods --ignore *.framework -w "%s" %s' % ('|'.join(search_word_list), ' '.join(search_dir_list))
+    lines = []
+    try:
         result = os.popen(command).read().strip()
         lines = result.split('\n')
-        for line in lines:
-            if line == '':
-                continue
-            matched_line = line.strip()
-            result_finditer1 = matched_line.startswith('@implementation %s' % cls)
-            if result_finditer1:
-                exist = True
-                break
-        if exist:
-            temp.append(cls)
-        print(cls, '存在' if exist else '不存在')
-    return temp
-
+    except:
+        print(command)
+    result_list = []
+    for line in lines:
+        if line == '':
+            continue
+        for className in search_word_list:
+            if className in line:
+                result_finditer2 = line.find('@implementation ' + className)
+                temp = line.split(':')
+                content = line
+                if len(temp) >= 3:
+                    content = temp[2]
+                if (result_finditer2 >= 0 and
+                    content.startswith('//') == False):
+                    if not className in result_list:
+                        result_list.append(className)
+                    break
+    return result_list
 
 def check_result_with_xml(modules_dir='', classes=None):
     if classes is None:
         return []
     temp = []
-    for cls in classes:
-        unused = True
-        command = 'ag -G ".+\.xml" --vimgrep "<%s " %s' % (cls, modules_dir)
+    command = 'ag -G ".+\.xml" --vimgrep -w "%s" %s' % ('|'.join(classes), modules_dir)
+    lines = []
+    try:
         result = os.popen(command).read().strip()
         lines = result.split('\n')
-        for line in lines:
-            if line == '':
-                continue
-            # 输出格式: 文件路径:行号:列号:匹配内容
-            parts = line.split(':', 3)
-            if len(parts) < 4:
-                continue
-            # file_path: 文件路径、
-            # line_number: 行号、
-            # column_number: 列号
-            # matched_line: 具体匹配内容
-            file_path, line_number, column_number, matched_line = parts
-            if matched_line.strip().startswith("<%s " % cls):
-                # FlexXmlBaseView。根view类型和类名一致
-                # 如果不是FlexXmlBaseView类型，可以认为此cls在xml中是有用的，所以需要从无用列表内过滤掉
-                filename_without_extension = os.path.splitext(os.path.basename(file_path))[0]
-                if filename_without_extension != cls:
-                    unused = False
-                    break
-        if unused:
-            temp.append(cls)
+    except:
+        print(command)
+    for line in lines:
+        if line == '':
+            continue
+        unused = True
+        for cls in classes:
+            if cls in line:
+                # 输出格式: 文件路径:行号:列号:匹配内容
+                parts = line.split(':', 3)
+                if len(parts) < 4:
+                    continue
+                # file_path: 文件路径、
+                # line_number: 行号、
+                # column_number: 列号
+                # matched_line: 具体匹配内容
+                file_path, line_number, column_number, matched_line = parts
+                if matched_line.strip().startswith("<%s " % cls):
+                    # FlexXmlBaseView。根view类型和类名一致
+                    # 如果不是FlexXmlBaseView类型，可以认为此cls在xml中是有用的，所以需要从无用列表内过滤掉
+                    filename_without_extension = os.path.splitext(os.path.basename(file_path))[0]
+                    if filename_without_extension != cls:
+                        unused = False
+                        break
+                if unused:
+                    temp.append(cls)
     return temp
 
 
@@ -72,14 +106,21 @@ def check_result_with_plist(modules_dir='', classes=None):
     if classes is None:
         return []
     temp = []
-    for cls in classes:
-        command = 'ag -c -G ".+\.plist" -w "%s" %s' % (cls, modules_dir)
+    command = 'ag -G ".+\.plist" -w "%s" %s' % ('|'.join(classes), modules_dir)
+    lines = []
+    try:
         result = os.popen(command).read().strip()
-        # 如果 result 为空，说明在plist中未找到类名，认为该类未使用
-        if not result:
-            temp.append(cls)
+        lines = result.split('\n')
+    except:
+        print(command)
+    for line in lines:
+        if line == '':
+            continue
+        for cls in classes:
+            if cls in line:
+                if not cls in temp:
+                    temp.append(cls)
     return temp
-
 
 def check_unuse_class_new(project_dir='', modules_dir='', scheme_name='', ipa_path=''):
     start_time = time.time()
@@ -90,7 +131,7 @@ def check_unuse_class_new(project_dir='', modules_dir='', scheme_name='', ipa_pa
     print(len(classes))
     print("duration：", time.time() - start_time)
 
-    classes = exists_in_private_modules(modules_dir, classes)
+    classes = chunk_exists_in_private_modules(classes, [modules_dir])
     for cls in classes:
         print(cls)
     print(len(classes))
